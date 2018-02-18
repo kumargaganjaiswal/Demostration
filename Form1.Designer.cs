61459ca8bc24b601495ce273dd44bb003577d8b8
Arun Kumar <kumararunjaiswal@outlook.com>
2018-02-19 00:01:57 +0530

    Added new text box for Age.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,11 +31,12 @@
             this.btnClick = new System.Windows.Forms.Button();
             this.txtMsg = new System.Windows.Forms.TextBox();
             this.lbltext = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnClick
             // 
-            this.btnClick.Location = new System.Drawing.Point(96, 76);
+            this.btnClick.Location = new System.Drawing.Point(191, 113);
             this.btnClick.Name = "btnClick";
             this.btnClick.Size = new System.Drawing.Size(75, 23);
             this.btnClick.TabIndex = 0;
@@ -53,17 +54,25 @@
             // lbltext
             // 
             this.lbltext.AutoSize = true;
-            this.lbltext.Location = new System.Drawing.Point(96, 140);
+            this.lbltext.Location = new System.Drawing.Point(264, 218);
             this.lbltext.Name = "lbltext";
             this.lbltext.Size = new System.Drawing.Size(25, 13);
             this.lbltext.TabIndex = 2;
             this.lbltext.Text = "......";
             // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(253, 39);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 20);
+            this.txtAge.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(287, 261);
+            this.ClientSize = new System.Drawing.Size(635, 375);
+            this.Controls.Add(this.txtAge);
             this.Controls.Add(this.lbltext);
             this.Controls.Add(this.txtMsg);
             this.Controls.Add(this.btnClick);
@@ -79,6 +88,7 @@
         private System.Windows.Forms.Button btnClick;
         private System.Windows.Forms.TextBox txtMsg;
         private System.Windows.Forms.Label lbltext;
+        private System.Windows.Forms.TextBox txtAge;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -19,7 +19,7 @@ namespace ApplicationMaster
 
         private void button1_Click(object sender, EventArgs e)
         {
-           lbltext.Text=txtMsg.Text;
+            lbltext.Text = "Hi.." + txtMsg.Text + ", your Age is " + txtAge.Text+".";
         }
     }
 }
