667f3c5fdad002ebf3f9e26335a3a57da0d8dd0d
Arun Kumar <kumararunjaiswal@outlook.com>
2018-02-18 23:27:46 +0530

    Added MessageBox

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace ApplicationMaster
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
     }
 }
